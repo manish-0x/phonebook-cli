df4ea4a859cc36f499d23f4b0c20e9f6b2a150dc
Manish Singh <165829122+manish-0x@users.noreply.github.com>
2026-01-03 23:02:18 +0530

    Decided initial basic program structure using comments

--- phonebook.c
+++ phonebook.c
@@ -1,1 +1,7 @@
 # Phonebook program source code
+
+# Use file handling with a text file to save, edit and retrieve data
+
+# Simple CLI application basic layout with keyboard as primary input
+
+# File handling using fstream
