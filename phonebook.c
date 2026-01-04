951d486672f45343b5d80f211a8c4e21c4cb29b9
Manish Singh <165829122+manish-0x@users.noreply.github.com>
2026-01-04 23:24:51 +0530

    Fixed comment syntax to proper C style comment syntax
    Coding in python recently left me with a habit of starting comments with '#'. Fixed it by correctly starting the comments for this C file with "//"

--- phonebook.c
+++ phonebook.c
@@ -1,7 +1,7 @@
-# Phonebook program source code
+// Phonebook program source code
 
-# Use file handling with a text file to save, edit and retrieve data
+// Use file handling with a text file to save, edit and retrieve data
 
-# Simple CLI application basic layout with keyboard as primary input
+// Simple CLI application basic layout with keyboard as primary input
 
-# File handling using fstream
+// File handling using fstream
