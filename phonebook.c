da074a033345897035ee674dd076576ae871c656
Manish Singh <165829122+manish-0x@users.noreply.github.com>
2026-01-05 23:38:00 +0530

    Started the actual code

--- phonebook.c
+++ phonebook.c
@@ -1,7 +1,11 @@
 // Phonebook program source code
+#include <stdio.h>
+
+int main(void)
+{
+  return 0
+}
 
 // Use file handling with a text file to save, edit and retrieve data
 
 // Simple CLI application basic layout with keyboard as primary input
-
-// File handling using fstream
