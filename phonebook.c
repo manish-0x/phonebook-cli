9aabbc4e8f35019aa2887da7087c933a310b2d47
Manish Singh <165829122+manish-0x@users.noreply.github.com>
2026-01-10 23:53:30 +0530

    Started menu driver implementation

--- phonebook.c
+++ phonebook.c
@@ -13,6 +13,7 @@ typedef struct contact
 }contact;
 
 void menu();
+void show();
 void add(contact*);
 void search(char* key, char* value);
 void update(contact*);
@@ -20,16 +21,45 @@ void remove(contact*);
 
 int main(void)
 {
+  int choice = 0;
+  
   menu();
+  do
+  {
+    scanf("%d", &choice);
+  }
+  while (choice < 1 && choice > 5)
+
+  // TODO: Add menu driver implimentation
+  switch (choice)
+  {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
 }
 
 void menu()
 {
-  printf("********************\n";
+  printf("********************\n");
   printf("     Phone Book     \n");
-  printf("********************\") n;
+  printf("********************\n");
 
-  printf("Choose an option and enter it to continue\n");
+  printf("Choose an option and enter it to continue\n\n");
+  
+  printf("1. Show contacts\n")
+  printf("2. Add contact\n");
+  printf("3. Search contact\n");
+  printf("4. Edit contact\n");
+  printf("5. Delete contact\n");
+
+  printf("\nChoice: ");
 }
 
 // Use file handling with a text file to save, edit and retrieve data
